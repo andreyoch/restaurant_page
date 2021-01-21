7bc74ad63ad66722648c4252519a2b1bb79e4184
Andrew Tsishevskii <at100991@student.sgh.waw.pl>
2021-01-21 19:04:36 +0200

    Do header render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import { header } from './header';
 import { mainPage } from './main-page';
 import { teamPage } from './team-page';
-import {menuPage} from './menu-page';
-import {contactPage} from './contact-page'
-
+import { menuPage } from './menu-page';
+import { contactPage } from './contact-page';
 
 function rednerSite(...pages) {
+  const body = document.querySelector('body');
   const content = document.querySelector('#content');
+
+  body.insertBefore(header, content);
+  content.insertBefore;
+
   for (let i = 0; i < pages.length; i++) {
     content.append(pages[i]);
   }
 }
 
-rednerSite(mainPage,teamPage,menuPage,contactPage)
-
+rednerSite(mainPage, teamPage, menuPage, contactPage);
