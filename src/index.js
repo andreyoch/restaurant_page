5589d9001523ba2804d164a810aa65f250f820da
Andrew Tsishevskii <at100991@student.sgh.waw.pl>
2021-01-21 19:24:28 +0200

    Do tab switching function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { teamPage } from './team-page';
 import { menuPage } from './menu-page';
 import { contactPage } from './contact-page';
 
-function rednerSite(...pages) {
+function renderSite(...pages) {
   const body = document.querySelector('body');
   const content = document.querySelector('#content');
 
@@ -16,4 +16,69 @@ function rednerSite(...pages) {
   }
 }
 
-rednerSite(mainPage, teamPage, menuPage, contactPage);
+renderSite(mainPage, teamPage, menuPage, contactPage);
+
+function activateTabSwitching() {
+  const headerLogo = document.querySelector('.header_logo');
+  const ourTeamLink = document.querySelector('#our-team');
+  const menuLink = document.querySelector('#menu');
+  const contactLink = document.querySelector('#contact');
+  const menuBtn = document.querySelector('.menu-btn_row');
+
+  ourTeamLink.addEventListener('click', () => {
+    mainPage.style = 'display: none';
+    menuPage.style = 'display: none';
+    contactPage.style = 'display: none';
+    teamPage.style = 'display:block';
+
+    ourTeamLink.classList.add('active-tab');
+    menuLink.classList.remove('active-tab');
+    contactLink.classList.remove('active-tab');
+  });
+
+  menuLink.addEventListener('click', () => {
+    mainPage.style = 'display: none';
+    teamPage.style = 'display: none';
+    contactPage.style = 'display: none';
+    menuPage.style = 'display: block';
+
+    menuLink.classList.add('active-tab');
+    ourTeamLink.classList.remove('active-tab');
+    contactLink.classList.remove('active-tab');
+  });
+
+  contactLink.addEventListener('click',() => {
+    mainPage.style = 'display: none';
+    teamPage.style = 'display: none';
+    menuPage.style = 'display: none';
+    contactPage.style = 'display: block';
+
+    contactLink.classList.add('active-tab');
+    menuLink.classList.remove('active-tab');
+    ourTeamLink.classList.remove('active-tab');
+  })
+
+  headerLogo.addEventListener('click',() => {
+    teamPage.style = 'display: none';
+    menuPage.style = 'display: none';
+    contactPage.style = 'display: none';
+    mainPage.style = 'display: block';
+
+    contactLink.classList.remove('active-tab');
+    menuLink.classList.remove('active-tab');
+    ourTeamLink.classList.remove('active-tab');
+  })
+
+  menuBtn.addEventListener('click',() => {
+    mainPage.style = 'display: none';
+    teamPage.style = 'display: none';
+    contactPage.style = 'display: none';
+    menuPage.style = 'display: block';
+
+    menuLink.classList.add('active-tab');
+    ourTeamLink.classList.remove('active-tab');
+    contactLink.classList.remove('active-tab');
+  })
+}
+
+activateTabSwitching();
